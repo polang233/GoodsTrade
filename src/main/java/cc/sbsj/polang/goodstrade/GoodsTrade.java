dbfd76a58511c0fe62465cb2068054a873ad0470
Polang <polang233@qq.com>
2026-03-10 17:02:39 +0800

    Initial commit

---
+++ src/main/java/cc/sbsj/polang/goodstrade/GoodsTrade.java
@@ -0,0 +1,17 @@
+package cc.sbsj.polang.goodstrade;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GoodsTrade extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
